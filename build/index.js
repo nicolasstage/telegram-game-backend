a885513139b5262a6d8354a4c87884a5d7248ba5
Nícolas Maia <nicolas.maia@stage.consulting>
2024-07-11 10:39:39 -0300

    chore: remove build folder from git

--- build/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./localServer/index");
-const port = parseInt(process.argv[2]) || 3001;
-new index_1.Daemon(port, '');
