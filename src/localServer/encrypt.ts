f0d5e015872e86147d8e6a2c65739474e59d71d0
nicolasstage <nicolas.maia@stage.consulting>
2024-10-15 18:21:37 -0300

    chore: enable miningV2

--- src/localServer/encrypt.ts
+++ src/localServer/encrypt.ts
@@ -106,7 +106,7 @@ const processCmd = async (cmd: worker_command) => {
     }
 
     case "startMining": {
-      return startMiningV1(cmd);
+      return startMiningV2(cmd);
     }
 
     case "stopMining": {
